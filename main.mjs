180123926d540f5bf1b301a74784d0a112d2188d
Simon Brulhart <simon@brulhart.me>
2024-09-10 19:45:11 +0200

    Try to import multiple modules
    Unfortunately, preact/compat needs an importmap to import preact,
    and importmaps seem a bit flaky in bookmarklets (e.g. there
    can be at most one inline importmap per page, and it cannot be
    linked to a remote file with src).

--- main.mjs
+++ main.mjs
@@ -1,10 +1,26 @@
-import {
-  useRef,
-  html,
-  useEffect,
-  useState,
-  render,
-} from "./vendor/htm@3.1.1/preact/standalone.module.js";
+// import { html } from "./vendor/htm@3.1.1/dist/htm.mjs";
+// import { render } from "./vendor/preact@10.23.2/dist/preact.mjs"
+// import { useRef, useEffect, useState } from "./vendor/preact@10.23.2/compat/dist/compat.mjs"
+
+elem = document.createElement("script");
+elem.type = "importmap";
+elem.innerHTML = `
+  {
+    "imports": {
+      "preact": "https://esm.sh/preact@10.23.1",
+      "preact/": "https://esm.sh/preact@10.23.1/",
+      "react": "https://esm.sh/preact@10.23.1/compat",
+      "react/": "https://esm.sh/preact@10.23.1/compat/",
+      "react-dom": "https://esm.sh/preact@10.23.1/compat",
+    }
+  }
+`;
+document.head.appendChild(elem);
+
+import { html } from "./vendor/htm@3.1.1/dist/htm.mjs";
+import { render } from "preact"
+import { useRef, useEffect, useState } from "preact/compat"
+
 
 function getCsrfToken() {
   const token = document.querySelector('meta[name="csrf-token"]').content;
